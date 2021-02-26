1665453a36bc22504bfaf5f684f3b10c16380fc8
Nguyen Hoang Cong Nguyen <congnguyen008@gmail.com>
2021-02-26 19:38:33 +0700

    add base

---
+++ src/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+    >
+    <thong-tin/>
+  </q-page>
+</template>
+
+<script>
+import ThongTin from './components/ThongTin.vue'
+export default {
+  components: { ThongTin },
+  name: 'PageIndex'
+}
+</script>
---
+++ src/pages/components/ThongTin.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Thông tin test</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
